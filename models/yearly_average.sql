658e5e0a06053e8a782b5692ab6677cfe97e9df6
roasfora <roasfora@gmail.com>
2025-02-12 11:57:15 +0000

    atualizar arquivos do dbt

---
+++ models/yearly_average.sql
@@ -0,0 +1,15 @@
+WITH yearly_data AS (
+    SELECT 
+        DATE_PART('year', date) AS year,
+        AVG(open) AS avg_open,
+        AVG(high) AS avg_high,
+        AVG(low) AS avg_low,
+        AVG(close) AS avg_close,
+        AVG(adjusted_close) AS avg_adjusted_close,
+        AVG(volume) AS avg_volume,
+        AVG(dividend_amount) AS avg_dividend
+    FROM public.monthly_adjusted_data
+    GROUP BY year
+    ORDER BY year
+)
+SELECT * FROM yearly_data
